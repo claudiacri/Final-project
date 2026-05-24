ecd684a80409fe865a2d6bea9acbcdc5ea812460
cldcr <nitepm1@gmail.com>
2026-05-24 22:40:36 +0200

    FINAL PROJECT: Update project structure Adding structure

---
+++ .idea/src/main/java/com/example/cakeme/CakeMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cakeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakeMeApplication.class, args);
+    }
+
+}
