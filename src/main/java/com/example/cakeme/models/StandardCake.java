42ddeef8903da6d1689d825e6e806ae15c317cc1
cldcr <nitepm1@gmail.com>
2026-05-26 18:01:50 +0200

    26.5.26 models

---
+++ src/main/java/com/example/cakeme/models/StandardCake.java
@@ -0,0 +1,38 @@
+package com.example.cakeme.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "standard_cake")
+@PrimaryKeyJoinColumn(name = "cake_id")
+public class StandardCake extends Cake {
+
+    @Column(name = "is_dietary")
+    private Boolean isDietary;
+
+    private String allergens;
+
+    @Column(name = "available_in_stock")
+    private Integer availableInStock;
+
+    public StandardCake() {}
+
+    public StandardCake(String name, String description, Double basePrice, String imageUrl,
+                        PastryShop pastryShop, Boolean isDietary, String allergens, Integer availableInStock) {
+        super(name, description, basePrice, imageUrl, pastryShop);
+        this.isDietary = isDietary;
+        this.allergens = allergens;
+        this.availableInStock = availableInStock;
+    }
+
+    // Getter e Setter
+    public Boolean getIsDietary() { return isDietary; }
+    public void setIsDietary(Boolean dietary) { isDietary = dietary; }
+
+    public String getAllergens() { return allergens; }
+    public void setAllergens(String allergens) { this.allergens = allergens; }
+
+    public Integer getAvailableInStock() { return availableInStock; }
+    public void setAvailableInStock(Integer availableInStock) { this.availableInStock = availableInStock; }
+}
---
+++ src/test/java/com/example/cakeme/CakeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cakeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
